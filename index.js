247665c641623d0b213facb386a11e4f44dd0422
AlexVillasenor13 <isaac.villasenor2921@alumnos.udg.mx>
2021-04-02 13:34:27 -0600

    leccion 4

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 var express = require('express');
-var socket = require('socket.io')
- 
-//App setup
+var socket = require('socket.io');
+
+// App setup
 var app = express();
 var server = app.listen(4000, function(){
-    console.log('listening to requests on prt 4000');
+    console.log('listening for requests on port 4000,');
 });
 
-// Staticfiles
+// Static files
 app.use(express.static('public'));
 
-// Socket setup
+// Socket setup & pass server
 var io = socket(server);
+io.on('connection', (socket) => {
+
+    console.log('made socket connection', socket.id);
+
+    // Handle chat event
+    socket.on('chat', function(data){
+        // console.log(data);
+        io.sockets.emit('chat', data);
+    });
+
+    // Handle typing event
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
+    });
+
+});
